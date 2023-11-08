bb18ec8a341e41ca9bc8509416069f1391114e49
Sileshi Abebe <abebesileshi828@gmail.com>
2023-11-08 11:56:57 +0300

    implemeted domain

---
+++ A2SV.ProductHubManagement.Domain/Category.cs
@@ -0,0 +1,18 @@
+﻿using A2SV.ProductHubManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2SV.ProductHubManagement.Domain
+{
+    public class Category:BaseDomainEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        // Navigation property for one-to-many relationship with products
+        public ICollection<Product> Products { get; set; }
+    }
+}
